cdac38edc449684a453c589431f506c52e382756
Chris Athanas <realityexpander@gmail.com>
2022-08-14 02:39:03 -0500

    initial commit

---
+++ app/src/main/java/com/realityexpander/observeconnectivity/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.realityexpander.observeconnectivity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.realityexpander.observeconnectivity.ui.theme.ObserveConnectivityTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var connectivityObserver: ConnectivityObserver
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        connectivityObserver = NetworkConnectivityObserver(applicationContext)
+        setContent {
+            ObserveConnectivityTheme {
+                val status by connectivityObserver.observe().collectAsState(
+                    initial = ConnectivityObserver.Status.Unavailable
+                )
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "Network status: $status")
+                }
+            }
+        }
+    }
+}
